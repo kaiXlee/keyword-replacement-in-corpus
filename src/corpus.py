39dbf45e9b51501921f86acc13b2ed7b50eb1375
kaiXlee <kyle96921@hotmail.com>
2021-03-24 21:04:05 -0700

    added verbose print, modified tryItABunch to perform multiple trials, added sanity check

--- src/corpus.py
+++ src/corpus.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 import time
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 
@@ -10,18 +11,14 @@ def loadSlang(fName):
     with open(fName) as file:
         for line in file:
             if(line!="\n"):
-                # strip trailing newline
-                line = line.strip("\n")
+                line = line.strip("\n") # strip trailing newline
                 temp = line.split("\t")
                 slangDict.update({temp[0]: temp[1]})
     return slangDict
 
 def loadCorpusDf(fName):
     df = pd.read_csv(fName)
-    # print(df.head())
     texts = df["text"]
-    # for text in texts:
-        # print(text)
     return texts
 
 def processMultipleTexts(texts, slangDict, wordCount=float('inf'), wordsProcessedCount = 0, slangCount = 0):
@@ -29,11 +26,10 @@ def processMultipleTexts(texts, slangDict, wordCount=float('inf'), wordsProcesse
         if(wordsProcessedCount >= wordCount):
             break
         (wordsProcessedCount, slangCount) = processStr(text, slangDict, wordCount, wordsProcessedCount, slangCount)
-    # print("wordsProcessedCount: "+str(wordsProcessedCount))
     return wordsProcessedCount, slangCount
 
 
-def processStr(string, slangDict, wordCount=float('inf'), wordsProcessedCount=0, slangCount=0):
+def processStr(string, slangDict, wordCount=float('inf'), wordsProcessedCount=0, slangCount=0, verbose=False):
     words = string.split(" ")
     for i, word in enumerate(words):
         if(wordsProcessedCount >= wordCount):
@@ -41,20 +37,24 @@ def processStr(string, slangDict, wordCount=float('inf'), wordsProcessedCount=0,
         # key in dictionary has a time complexity of O(1) according to https://wiki.python.org/moin/TimeComplexity
         if word in slangDict:
             slangCount += 1
+            words[i] = slangDict[word]
+            if verbose:
+                print(f"\"{word}\" is replaced with: \"{slangDict[word]}\"")
         wordsProcessedCount += 1
-    return wordsProcessedCount, slangCount
+    return wordsProcessedCount, slangCount, words
 
 
 def tryItABunch(myFunc, texts , slangDict, startN=10, endN=100, stepSize=10, numTrials=20, listMax = 10):
-    nValues = []
+    nValues = [n for n in range(startN, endN, stepSize)]
     timeValues = []
     for n in range(startN, endN, stepSize):
-        startTime = time.time()
-        myFunc(texts, slangDict, n)
-        endTime = time.time()
-        runtime = (endTime - startTime) * 1000 # measured in milliseconds
-        nValues.append(n)
-        timeValues.append(runtime)
+        runtime = 0
+        for x in range(0, numTrials):
+            startTime = time.time()
+            myFunc(texts, slangDict, n)
+            endTime = time.time()
+            runtime += (endTime - startTime) * 1000 # measured in milliseconds
+        timeValues.append(runtime/numTrials)
     return nValues, timeValues
 
 
@@ -62,13 +62,18 @@ if __name__ == "__main__":
     slangDict = {}
     slangDict = loadSlang("../dataset/slangs.txt")
     texts = loadCorpusDf("../dataset/Android_Q/android-10_bd_2019-01-01-00-00-00_rd_2019-04-22-14-07-31.csv") # size = 48136
-    # processMultipleTexts(texts, slangDict, 1000)
+    
+    # Sanity Check: we expect text[2] has two matches. "a3" is replaced twice
+    (x, y, proccessedStr) = processStr( string = texts[2], slangDict = slangDict, wordCount = 100, verbose = True)
+    print("\nORIGINAL: "+texts[2])
+    print("\nPROCESSED: "+' '.join(proccessedStr))
 
-    # # text[2] has two matches. a3 is matched twice
-    # processStr( string = texts[2], slangDict = slangDict, wordCount = 100)
-    (nValues, timeValues) = tryItABunch(myFunc = processMultipleTexts, texts = texts, slangDict = slangDict, startN = 10000, endN = 48000, stepSize=1000)
-
+    # Plotting asymtotic time complexity
+    (nValues, timeValues) = tryItABunch(myFunc = processMultipleTexts, texts = texts, slangDict = slangDict, startN = 10000, endN = 48000, stepSize=1000, numTrials=50)
     plt.plot(nValues, timeValues, color="blue", label="test1")
+    z = np.polyfit(nValues, timeValues, 1)
+    p = np.poly1d(z)
+    plt.plot(nValues,p(nValues),"r--")
     plt.xlabel("n")
     plt.ylabel("Time(ms)")
     plt.legend()
