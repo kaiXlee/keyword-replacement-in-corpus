66fa9f8b3461a92d7cf7d008593ff07a23a1e036
kaiXlee <kyle96921@hotmail.com>
2021-04-08 03:04:33 -0700

    Milestone4 - tentative version

--- src/milestone4.py
+++ src/milestone4.py
@@ -22,23 +22,46 @@ def loadCorpusDf(fName):
     texts = df["text"]
     return texts
 
-def processMultipleTexts(texts, slangDict, wordCount=float('inf'), wordsProcessedCount = 0, slangCount = 0, countractionCount= 0):
+def processMultipleTexts(texts, slangDict, wordCount=float('inf'), wordsProcessedCount = 0, slangCount = 0):
     for i, text in enumerate(texts):
         if(wordsProcessedCount >= wordCount):
             break
         words = None
-        # (wordsProcessedCount, slangCount) = processStr(text, slangDict, wordCount, wordsProcessedCount, slangCount)
-        (wordsProcessedCount, contractionCount, words) = processStr2(text, contractions, wordCount, wordsProcessedCount, countractionCount)
-    # return wordsProcessedCount, slangCount
-    return wordsProcessedCount, contractionCount
+        (wordsProcessedCount, slangCount, words) = processStr(text, slangDict, wordCount, wordsProcessedCount, slangCount)
+    return wordsProcessedCount, slangCount
+
 
 def processStr(string, slangDict, wordCount=float('inf'), wordsProcessedCount=0, slangCount=0, verbose=False):
     words = string.split(" ")
+    hints = ["'"]   # " . " can be added but how to differenciate from a word ending in a sentence (eg. someWord.)
     for i, word in enumerate(words):
+        replaceEntireWord = True
         if(wordsProcessedCount >= wordCount):
             break
         # key in dictionary has a time complexity of O(1) according to https://wiki.python.org/moin/TimeComplexity
-        if word in slangDict:
+
+        # Check if word has apostrophy type abbreviation:
+        for j in range(len(hints)):
+            if hints[j] in word:
+                replaceEntireWord = False
+                index = word.index(hints[j])
+                substringShort = word[index:] # " 'll, 've, 're "
+                substringLong = word[index-1:]  # "n't"
+                #HashMap of abbreviations to full word. (eg. key="'t" and value="not")
+                if substringShort in slangDict:
+                    slangCount += 1
+                    words[i] = word.replace(substringShort, " " + slangDict[substringShort])
+                    if verbose:
+                        print(f"\"{word}\" is replaced with: \"{words[i]}\"")
+                elif substringLong in slangDict:
+                    slangCount += 1
+                    words[i] = word.replace(substringLong, " " + slangDict[substringLong])
+                    if verbose:
+                        print(f"\"{word}\" is replaced with: \"{words[i]}\"")
+                wordsProcessedCount += 1
+
+        # Check if entire word shoud be replaced
+        if replaceEntireWord and word in slangDict:
             slangCount += 1
             words[i] = slangDict[word]
             if verbose:
@@ -46,68 +69,38 @@ def processStr(string, slangDict, wordCount=float('inf'), wordsProcessedCount=0,
         wordsProcessedCount += 1
     return wordsProcessedCount, slangCount, words
 
-
-def processStr2(string, contractions, wordCount=float('inf'), wordsProcessedCount=0, contractionCount=0, verbose=False):
-
-    words = string.split(" ")
-    hints = ["'"]   # " . " can be added but how to differenciate from a word ending in a sentence (eg. someWord.)
-    for i, word in enumerate(words):
-        if(wordsProcessedCount >= wordCount):
-            break
-        for j in range(len(hints)):
-            if hints[j] in word:
-                index = word.index(hints[j])
-                substringShort = word[index:] # " 'll, 've, 're "
-                substringLong = word[index-1:]  # "n't"
-
-                #HashMap of abbreviations to full word. (eg. key="'t" and value="not")
-                if substringShort in contractions:
-                    contractionCount += 1
-                    words[i] = word.replace(substringShort, " " + contractions[substringShort])
-                    if verbose:
-                        print(f"\"{word}\" is replaced with: \"{words[i]}\"")
-                elif substringLong in contractions:
-                    contractionCount += 1
-                    words[i] = word.replace(substringLong, " " + contractions[substringLong])
-                    if verbose:
-                        print(f"\"{word}\" is replaced with: \"{words[i]}\"")
-        wordsProcessedCount += 1
-    return wordsProcessedCount, contractionCount, words
-
-
-def tryItABunch(myFunc, texts , contractions, startN=10, endN=100, stepSize=10, numTrials=20, listMax = 10):
+def tryItABunch(myFunc, texts , slangDict, startN=10, endN=100, stepSize=10, numTrials=20, listMax = 10):
     nValues = [n for n in range(startN, endN, stepSize)]
     timeValues = []
     for n in range(startN, endN, stepSize):
         runtime = 0
         for x in range(0, numTrials):
             startTime = time.time()
-            myFunc(texts, contractions, n)
+            myFunc(texts, slangDict, n)
             endTime = time.time()
-            runtime += (endTime - startTime) * 1000 # measured in milliseconds
+            runtime += (endTime - startTime) * 1000 # scale to milliseconds
+            # print(runtime)
         timeValues.append(runtime/numTrials)
     return nValues, timeValues
 
 if __name__ == "__main__":
-    slangDict = {}
     dirname = os.path.dirname(__file__)
-    contractions = loadSlang(os.path.join(dirname, '../dataset/contractions.txt'))
-    slangDict = loadSlang(os.path.join(dirname, '../dataset/slangs.txt'))
     texts = loadCorpusDf(os.path.join(dirname, "./../dataset/Android_Q/android-10_bd_2019-01-01-00-00-00_rd_2019-04-22-14-07-31.csv")) # size = 48136
-    #Check for contractions
-    (x, y, proccessedStr) = processStr2( string = texts[10], contractions = contractions, wordCount = 100, verbose=True)
+    
+    slangDict = {}
+    # Choose one of each files to use as a dictionary
+    slangsContractionsFile = '../dataset/slangs_and_contractions.txt'
+    slangsFile = '../dataset/slangs.txt'
+    chosenFile = os.path.join(dirname, slangsFile)
+    slangDict = loadSlang(chosenFile)
+
+    # Check for contractions
+    (x, y, proccessedStr) = processStr( string = texts[10], slangDict = slangDict, wordCount = 100, verbose=True)
     print("\nORIGINAL: "+texts[10])
     print("\nPROCESSED: "+' '.join(proccessedStr))
 
-    # # Sanity Check: we expect text[2] has two matches. "a3" is replaced twice
-    # (x, y, proccessedStr) = processStr( string = texts[2], slangDict = slangDict, wordCount = 100, verbose=True)
-    # print("\nORIGINAL: "+texts[2])
-    # print("\nPROCESSED: "+' '.join(proccessedStr))
-
     # Plotting asymtotic time complexity
-    # (nValues, timeValues) = tryItABunch(myFunc = processMultipleTexts, texts = texts, slangDict = slangDict, startN = 10000, endN = 48000, stepSize=1000, numTrials=50)
-    (nValues, timeValues) = tryItABunch(myFunc = processMultipleTexts, texts = texts, contractions = contractions, startN = 10000, endN = 48000, stepSize=1000, numTrials=50)
-
+    (nValues, timeValues) = tryItABunch(myFunc = processMultipleTexts, texts = texts, slangDict = slangDict, startN = 10000, endN = 48000, stepSize=1000, numTrials=50)
     plt.plot(nValues, timeValues, color="blue", label="test1")
 
     z = np.polyfit(nValues, timeValues, 1)
@@ -117,5 +110,5 @@ if __name__ == "__main__":
     plt.xlabel("n")
     plt.ylabel("Time(ms)")
     plt.legend()
-    plt.title("Corpus Transform")
+    plt.title("Corpus Transform\n" + os.path.basename(chosenFile))
     plt.show()
